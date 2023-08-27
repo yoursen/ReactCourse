495cbdaabf1368315c202baeaf6eb7aeb0bc2fa5
Yuriy Seniuk <yoursen@gmail.com>
2023-08-27 16:47:53 +0300

    Added filtering

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,4 +1,4 @@
-import { runInAction, makeAutoObservable } from "mobx"
+import { runInAction, makeAutoObservable, reaction } from "mobx"
 import { Activity, ActivityFormValues } from "../modules/activity";
 import agent from "../api/agent";
 import { format } from "date-fns";
@@ -14,9 +14,19 @@ export default class ActivityStore {
     loadingInitial = false;
     pagination: Pagination | null = null;
     pagingParams = new PagingParams();
+    predicate = new Map().set('all', true);
 
     constructor() {
         makeAutoObservable(this);
+
+        reaction(
+            () => this.predicate.keys(),
+            () => {
+                this.pagingParams = new PagingParams();
+                this.activityRegistry.clear();
+                this.loadActivities();
+            }
+        );
     }
 
     get activities() {
@@ -38,6 +48,13 @@ export default class ActivityStore {
         const params = new URLSearchParams();
         params.append('pageNumber', this.pagingParams.pageNumber.toString());
         params.append('pageSize', this.pagingParams.pageSize.toString());
+        this.predicate.forEach((value, key) => {
+            if (key == 'startDate') {
+                params.append(key, (value as Date).toISOString());
+            } else {
+                params.append(key, value);
+            }
+        });
         return params;
     }
 
@@ -62,6 +79,34 @@ export default class ActivityStore {
 
     setPagingParams = (pagingParams: PagingParams) => {
         this.pagingParams = pagingParams;
+    }
+
+    setPredicate = (predicate: string, value: string | Date) => {
+
+        const resetPredicate = () => {
+            this.predicate.forEach((value, key) => {
+                if (key !== 'startDate') this.predicate.delete(key);
+            });
+        };
+
+        switch (predicate) {
+            case 'all':
+                resetPredicate();
+                this.predicate.set('all', true);
+                break;
+            case 'isGoing':
+                resetPredicate();
+                this.predicate.set('isGoing', true);
+                break;
+            case 'isHost':
+                resetPredicate();
+                this.predicate.set('isHost', true);
+                break;
+            case 'startDate':
+                this.predicate.delete('startDate');
+                this.predicate.set('startDate', value);
+                break;
+        }
     }
 
     loadActivity = async (id: string) => {
--- client-app/src/features/activities/dashboard/ActivityFilters.tsx
+++ client-app/src/features/activities/dashboard/ActivityFilters.tsx
@@ -1,19 +1,32 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
 import Calendar from "react-calendar";
 import { Header, Menu } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
 
 
-export default function ActivityFilters() {
+export default observer(function ActivityFilters() {
+    const { activityStore: { predicate, setPredicate } } = useStore();
     return (<>
-        <Menu vertical size='large' style={{ width: '100%', marginTop:25 }}>
+        <Menu vertical size='large' style={{ width: '100%', marginTop: 25 }}>
             <Header icon='filter' attached color='teal' content='Filters' />
-            <Menu.Item content='All activities' />
-            <Menu.Item content='I am going' />
-            <Menu.Item content='I am hosting' />
+            <Menu.Item
+                active={predicate.has('all')}
+                onClick={() => setPredicate('all', 'true')}
+                content='All activities' />
+            <Menu.Item
+                active={predicate.has('isGoing')}
+                onClick={() => setPredicate('isGoing', 'true')}
+                content='I am going' />
+            <Menu.Item
+                active={predicate.has('isHost')}
+                onClick={() => setPredicate('isHost', 'true')}
+                content='I am hosting' />
         </Menu>
         <Header></Header>
-        <Calendar />
+        <Calendar
+            onChange={(date) => setPredicate('startDate', date as Date)}
+            value={predicate.get('startDate') || new Date()} />
     </>
-
     )
-}
+})
