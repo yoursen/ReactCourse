a2d197eb0df947571964aabd1a7bfce24e9ba58a
Yuriy Seniuk <yoursen@gmail.com>
2023-08-31 17:06:55 +0300

    added dev/prod api configuration

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -7,7 +7,7 @@ import { User, UserFormValues } from "../modules/user";
 import { Photo, Profile, UserActivity } from "../modules/profile";
 import { PaginatedResults } from "../modules/pagination";
 
-axios.defaults.baseURL = "http://localhost:5000/api";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 axios.interceptors.request.use(config => {
     const token = store.commonStore.token;
@@ -17,7 +17,7 @@ axios.interceptors.request.use(config => {
 });
 
 axios.interceptors.response.use(async response => {
-    await sleep(1000);
+    if (!import.meta.env.DEV) await sleep(1000);
 
     const pagination = response.headers["pagination"];
     if (pagination) {
--- client-app/src/features/errors/TestError.tsx
+++ client-app/src/features/errors/TestError.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import {Button, Header, Segment} from "semantic-ui-react";
+import { Button, Header, Segment } from "semantic-ui-react";
 import axios from 'axios';
 import ValidationError from './ValidationError';
 
 export default function TestErrors() {
-    const baseUrl = 'http://localhost:5000/api/'
+    const baseUrl = import.meta.env.VITE_API_URL;
     const [errors, setErrors] = useState(null);
 
 
@@ -29,7 +29,7 @@ export default function TestErrors() {
     }
 
     function handleValidationError() {
-        axios.post(baseUrl + 'activities', {}).catch(err => setErrors(err) );
+        axios.post(baseUrl + 'activities', {}).catch(err => setErrors(err));
     }
 
     return (
