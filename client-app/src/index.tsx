f4e73d8b53ca299fe3ccbeab0b2877a0f8a2d54b
Yuriy Seniuk <yoursen@gmail.com>
2023-08-31 16:49:26 +0300

    Migrated to vite

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,7 +5,6 @@ import 'react-calendar/dist/Calendar.css'
 import 'react-datepicker/dist/react-datepicker.css'
 import './app/layout/styles.css';
 import 'react-toastify/dist/ReactToastify.min.css'
-import reportWebVitals from './reportWebVitals';
 import { StoreContext, store } from './app/stores/store';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Route';
@@ -15,11 +14,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <StoreContext.Provider value={store}>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </StoreContext.Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
